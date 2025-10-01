c1714b3eccaa6e0cd062bdbfcd3f6e0b6d519470
Jany <62150940+Janybanny@users.noreply.github.com>
2025-10-02 01:49:38 +0200

    unpack superhtml directly

--- files/scripts/superhtml.sh
+++ files/scripts/superhtml.sh
@@ -3,5 +3,4 @@
 set -euo pipefail
 
 mkdir -p /tmp
-curl -L https://github.com/kristoff-it/superhtml/releases/latest/download/x86_64-linux-musl.tar.xz | tar -xvJf - -C /tmp/
-mv /tmp/x86_64-linux-musl/superhtml /usr/bin/superhtml
+curl -L https://github.com/kristoff-it/superhtml/releases/latest/download/x86_64-linux-musl.tar.xz | tar -xvJf - -C /usr/bin
