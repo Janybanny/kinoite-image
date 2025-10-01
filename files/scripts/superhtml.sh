d9e6d4ad6b767dd28a24501e58d9d5ac3ad0c888
Jany <62150940+Janybanny@users.noreply.github.com>
2025-10-02 01:34:58 +0200

    fix superhtml try 2

--- files/scripts/superhtml.sh
+++ files/scripts/superhtml.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 mkdir -p /tmp
-curl -L https://github.com/kristoff-it/superhtml/releases/latest/download/x86_64-linux-musl.tar.xz | tar -xvfJ - -C /tmp/
+curl -L https://github.com/kristoff-it/superhtml/releases/latest/download/x86_64-linux-musl.tar.xz | tar -xvJf - -C /tmp/
 mv /tmp/x86_64-linux-musl/superhtml /usr/bin/superhtml
